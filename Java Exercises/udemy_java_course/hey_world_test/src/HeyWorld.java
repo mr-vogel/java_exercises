6f4802150d03c96c9651bd64eb2b5171d7ad2af8
mr-vogel <vogel1mr@gmail.com>
2019-07-12 12:57:37 -0500

    updated HeyWorld project

--- Java Exercises/udemy_java_course/hey_world_test/src/HeyWorld.java
+++ Java Exercises/udemy_java_course/hey_world_test/src/HeyWorld.java
@@ -1,6 +1,18 @@
 public class HeyWorld {
 
     public static void main(String[] args) {
-        System.out.println("Test");
+
+        System.out.println("Hello, Mike!");
+
+        int myFirstNumber = (10 + 5) + (2 * 10);
+        int mySecondNumber = 12;
+        int myThirdNumber = myFirstNumber * 2;
+
+        int myTotal = myFirstNumber + mySecondNumber + myThirdNumber;
+
+        int difference = 1000 - myTotal;
+
+        System.out.println(difference);
+
     }
 }
