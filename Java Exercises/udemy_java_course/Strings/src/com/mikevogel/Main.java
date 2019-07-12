8e469db9221b730b19882f3c6ea4063447eab3f5
mr-vogel <vogel1mr@gmail.com>
2019-07-12 15:54:35 -0500

    add Strings project for udemy course

---
+++ Java Exercises/udemy_java_course/Strings/src/com/mikevogel/Main.java
@@ -0,0 +1,23 @@
+package com.mikevogel;
+
+public class Main {
+
+    public static void main(String[] args) {
+        // byte
+        // short
+        // int
+        // long
+        // float
+        // double
+        // char
+        // boolean
+        // all of those above data types are primitive data types
+
+        String myString = "This is a string";
+        System.out.println(myString);
+
+        String numberString = "250.55";
+        numberString = numberString + "49.95";
+        System.out.println(numberString);
+    }
+}
