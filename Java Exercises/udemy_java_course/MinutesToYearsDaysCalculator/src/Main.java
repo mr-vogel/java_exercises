98d4ff744bcbea7754e2af43896114ad09a11530
mr-vogel <vogel1mr@gmail.com>
2019-07-23 10:51:55 -0500

    added MinutesToYearsDaysCalculator exercise

---
+++ Java Exercises/udemy_java_course/MinutesToYearsDaysCalculator/src/Main.java
@@ -0,0 +1,17 @@
+public class Main {
+
+    public static void main(String[] args) {
+        MinutesToYearsDaysCalculator.printYearsAndDays(525600);
+        MinutesToYearsDaysCalculator.printYearsAndDays(1051200);
+        MinutesToYearsDaysCalculator.printYearsAndDays(561600);
+
+    }
+}
+
+/*
+ * printYearsAndDays(525600);  → should print "525600 min = 1 y and 0 d"
+
+ * printYearsAndDays(1051200); → should print "1051200 min = 2 y and 0 d"
+
+ * printYearsAndDays(561600);  → should print "561600 min = 1 y and 25 d"
+ */
