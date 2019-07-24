a990903acf0cb16775dd71bb7ee2ccb3319525fb
mr-vogel <vogel1mr@gmail.com>
2019-07-24 08:37:24 -0500

    add Sum3And5Challenge project

---
+++ Java Exercises/udemy_java_course/Sum3And5Challenge/src/com/mikevogel/Main.java
@@ -0,0 +1,27 @@
+package com.mikevogel;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        // Create a for statement using a range of number from 1 to 1000
+        // Sum all the numbers that can be divided with both 3 and also with 5
+        // For those numbers that met the above conditions, print out the number
+        // break out of the loop once you find 5 numbers that met the above conditions
+        // after breaking out of the loop print the sum of the numbers that met
+        // the above conditions
+        int sum = 0;
+        int counter = 0;
+        for (int i = 1; i <= 1000; i++) {
+            if ((i % 3 == 0) && (i % 5 == 0)) {
+                sum += i;
+                counter++;
+                System.out.println("Found number = " + i);
+            }
+            if (counter == 5) break;
+        }
+
+        System.out.println("Sum = " + sum);
+
+    }
+}
