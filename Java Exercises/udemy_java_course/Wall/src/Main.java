a7f7290620d3ea375b62fc379db9f78435c8b5f4
mr-vogel <vogel1mr@gmail.com>
2019-07-31 10:26:39 -0500

    added Wall exercise

---
+++ Java Exercises/udemy_java_course/Wall/src/Main.java
@@ -0,0 +1,30 @@
+public class Main {
+
+    public static void main(String[] args) {
+        Wall wall = new Wall(5,4);
+        System.out.println("area= " + wall.getArea());
+        wall.setHeight(-1.5);
+        System.out.println("width= " + wall.getWidth());
+        System.out.println("height= " + wall.getHeight());
+        System.out.println("area= " + wall.getArea());
+    }
+
+}
+
+/*
+→ TEST CODE:
+
+Wall wall = new Wall(5,4);
+System.out.println("area= " + wall.getArea());
+wall.setHeight(-1.5);
+System.out.println("width= " + wall.getWidth());
+System.out.println("height= " + wall.getHeight());
+System.out.println("area= " + wall.getArea());
+
+→ OUTPUT:
+
+area= 20.0
+width= 5.0
+height= 0.0
+area= 0.0
+ */
